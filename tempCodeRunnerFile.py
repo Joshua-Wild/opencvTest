b01ff5aed462177983a296699ed14058b5c6f8a7
joshua wild <joshua.wild@student.manchester.ac.uk>
2024-11-07 11:52:47 +0000

    2

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# def side(processed_landmark_list, classifier, bebop, current_time):
+#     hand_sign_id = classifier(processed_landmark_list)
+    
+#     # Control the drone based on the gesture
+#     control_drone_with_gesture(hand_sign_id, bebop, current_time)
